0dd3fcaeb44c32fbb8682ae645ef22230623ee1c
Hunkarada <hfsihtr@gmail.com>
2023-11-12 01:34:33 +0500

    remove redurant keywords

--- src/main/java/hunkarada/nen/common/abstractions/Registry.java
+++ src/main/java/hunkarada/nen/common/abstractions/Registry.java
@@ -1,6 +1,6 @@
 package hunkarada.nen.common.abstractions;
 
 public interface Registry<T> {
-    public abstract void addToRegistry(String key, T value);
-    public abstract <T> T getFromRegistry(String key);
+    void addToRegistry(String key, T value);
+    <T> T getFromRegistry(String key);
 }
