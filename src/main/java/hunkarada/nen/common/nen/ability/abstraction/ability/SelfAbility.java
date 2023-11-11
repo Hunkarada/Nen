c2a91616431625ce410c4f8c321b0045c48e8f9f
Hunkarada <hfsihtr@gmail.com>
2023-11-12 01:31:18 +0500

    test

--- src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/SelfAbility.java
+++ src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/SelfAbility.java
@@ -5,9 +5,5 @@ import net.minecraft.entity.LivingEntity;
 public abstract class SelfAbility extends Ability {
     @Override
     public void cast(LivingEntity caster){
-        // AMOGUS
-        int D =0;
-        int E =0;
-        // adfghjk;afghjko;adfgiohp
     }
 }
