4283e99f8f37bb6c3664c013de47a80ec1240991
Brilliance-DM <sobakaidi@mail.ru>
2023-11-12 01:23:29 +0500

    Bla bla V2

--- src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/SelfAbility.java
+++ src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/SelfAbility.java
@@ -6,6 +6,7 @@ public abstract class SelfAbility extends Ability {
     @Override
     public void cast(LivingEntity caster){
         int D =0;
+        int E =0;
         // adfghjk;afghjko;adfgiohp
     }
 }
