7cbc2838ad4da594cd1153fae92872bc90f260b8
Hunkarada <hfsihtr@gmail.com>
2023-11-16 07:50:08 +0500

    add new method to calculate multiplier, based on caster's NenType and ability NenType

--- src/main/java/hunkarada/nen/common/nen/NenType.java
+++ src/main/java/hunkarada/nen/common/nen/NenType.java
@@ -2,6 +2,9 @@ package hunkarada.nen.common.nen;
 
 import hunkarada.nen.common.abstractions.CanNbt;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public enum NenType implements CanNbt {
     ENHANCEMENT("Enhancement"), EMISSION("Emission"), MANIPULATION("Manipulation"), SPECIALIZATION("Specialization"), CONJURATION("Conjuration"), TRANSMUTATION("Transmutation"), UNIDENTIFIED("Unidentified");
     private final String id;
@@ -15,6 +18,147 @@ public enum NenType implements CanNbt {
     public String toNbt(){
         return this.toString();
     }
+    //stupid implementation with switch-case construction,
+    // as it will be much faster, than index calculations, and much simpler to do
+    public double calcTypeMultiplier(NenType anotherNenType) {
+        switch (this.id){
+            case "Enhancement" -> {
+                switch (anotherNenType.id){
+                    case "Enhancement" -> {
+                        return 1.0;
+                    }
+                    case "Emission" -> {
+                        return 0.8;
+                    }
+                    case "Manipulation" -> {
+                        return 0.6;
+                    }
+                    case "Specialization" -> {
+                        return 0.01;
+                    }
+                    case "Conjuration" -> {
+                        return 0.6;
+                    }
+                    case "Transmutation" -> {
+                        return 0.8;
+                    }
+                }
+            }
+            case "Emission" -> {
+                switch (anotherNenType.id){
+                    case "Enhancement" -> {
+                        return 0.8;
+                    }
+                    case "Emission" -> {
+                        return 1.0;
+                    }
+                    case "Manipulation" -> {
+                        return 0.8;
+                    }
+                    case "Specialization" -> {
+                        return 0.01;
+                    }
+                    case "Conjuration" -> {
+                        return 0.4;
+                    }
+                    case "Transmutation" -> {
+                        return 0.6;
+                    }
+                }
+
+            }
+            case "Manipulation" -> {
+                switch (anotherNenType.id){
+                    case "Enhancement" -> {
+                        return 0.6;
+                    }
+                    case "Emission" -> {
+                        return 0.8;
+                    }
+                    case "Manipulation" -> {
+                        return 1.0;
+                    }
+                    case "Specialization" -> {
+                        return 0.01;
+                    }
+                    case "Conjuration" -> {
+                        return 0.6;
+                    }
+                    case "Transmutation" -> {
+                        return 0.4;
+                    }
+                }
+
+            }
+            case "Specialization" -> {
+                switch (anotherNenType.id){
+                    case "Enhancement" -> {
+                        return 0.4;
+                    }
+                    case "Emission" -> {
+                        return 0.6;
+                    }
+                    case "Manipulation" -> {
+                        return 0.8;
+                    }
+                    case "Specialization" -> {
+                        return 1.0;
+                    }
+                    case "Conjuration" -> {
+                        return 0.8;
+                    }
+                    case "Transmutation" -> {
+                        return 0.6;
+                    }
+                }            }
+            case "Conjuration" -> {
+                switch (anotherNenType.id){
+                    case "Enhancement" -> {
+                        return 0.6;
+                    }
+                    case "Emission" -> {
+                        return 0.4;
+                    }
+                    case "Manipulation" -> {
+                        return 0.6;
+                    }
+                    case "Specialization" -> {
+                        return 0.01;
+                    }
+                    case "Conjuration" -> {
+                        return 1.0;
+                    }
+                    case "Transmutation" -> {
+                        return 0.8;
+                    }
+                }
+            }
+            case "Transmutation" -> {
+                switch (anotherNenType.id){
+                    case "Enhancement" -> {
+                        return 0.8;
+                    }
+                    case "Emission" -> {
+                        return 0.6;
+                    }
+                    case "Manipulation" -> {
+                        return 0.4;
+                    }
+                    case "Specialization" -> {
+                        return 0.01;
+                    }
+                    case "Conjuration" -> {
+                        return 0.8;
+                    }
+                    case "Transmutation" -> {
+                        return 1.0;
+                    }
+                }
+            }
+        }
+        throw new RuntimeException("NenType Error");
+    }
+
     public NenType fromNbt(String id){
         return switch (id) {
             case "Enhancement" -> NenType.ENHANCEMENT;
