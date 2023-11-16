e64b07fcd3033bf5c31e9c01642ef12930197daf
Hunkarada <hfsihtr@gmail.com>
2023-11-16 23:00:49 +0500

    reworked AbilityEffect

--- src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/AbilityEffect.java
+++ src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/AbilityEffect.java
@@ -2,48 +2,41 @@ package hunkarada.nen.common.nen.ability.abstraction.ability;
 
 import hunkarada.nen.common.abstractions.CanNbt;
 import hunkarada.nen.common.abstractions.CanRegister;
+import hunkarada.nen.common.nen.ability.abstraction.target.TargetType;
 import hunkarada.nen.common.nen.ability.registry.EffectRegistry;
+import hunkarada.nen.common.nen.mixin.INen;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.hit.HitResult;
 
 public abstract class AbilityEffect implements CanNbt, CanRegister {
     protected LivingEntity caster;
-    protected HitResult.Type targetType;
+    protected TargetType targetType;
     protected Block blockTarget;
     protected Entity entityTarget;
     protected String id;
     protected int duration;
     protected boolean isFirstTick = true;
-    protected <T> void applyEffect(T target, HitResult.Type targetType, LivingEntity caster){
-        applyVariables(target, targetType, this.caster);
-        if (targetType == HitResult.Type.ENTITY){
-            caster.writeToNenMemory();// write info about ability effect to entity/block method.
-            firstTickEffect(entityTarget);
-            isFirstTick = false;
-        }
-        else {
-            // write info about ability effect to entity/block method.
-            firstTickEffect(blockTarget);
-            isFirstTick = false;
-        }
+    protected void applyEffect(Entity target, LivingEntity caster){
+        prepareEffect(target, this.caster);
+        INen nenCaster = (INen) caster;
+        firstTickEffect(entityTarget);
+        isFirstTick = false;
     }
+    protected void applyEffect(Block target, LivingEntity caster){
 
+    }
+    protected abstract void firstTickEffect(Block target);
+    protected abstract void firstTickEffect(Entity target);
+    protected abstract void durationalEffect(Block target);
+    protected abstract void durationalEffect(Entity target);
 
-    protected abstract  <T> void firstTickEffect(T target);
-    protected abstract  <T> void durationalEffect(T target);
-
-    protected <T> void applyVariables(T target, HitResult.Type targetType, LivingEntity caster){
-        switch (targetType){
-            case ENTITY -> {
-                this.targetType = targetType;
-                this.entityTarget = (Entity) target;
-            }
-            case BLOCK -> {
-                this.targetType = targetType;
-                this.blockTarget = (Block) target;
-            }
+    protected <T> void prepareEffect(T target, LivingEntity caster){
+        if (target instanceof Entity) {
+            this.entityTarget = (Entity) target;
+        }
+        else if (target instanceof Block) {
+            this.blockTarget = (Block) target;
         }
         this.caster = caster;
     }
