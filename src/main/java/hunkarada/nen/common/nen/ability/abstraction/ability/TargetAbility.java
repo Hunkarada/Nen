5041d5f6bd6b06c174231f86c23ab34c4fd16f22
Hunkarada <hfsihtr@gmail.com>
2023-11-16 21:34:31 +0500

    fixed casting of TargetAbility

--- src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/TargetAbility.java
+++ src/main/java/hunkarada/nen/common/nen/ability/abstraction/ability/TargetAbility.java
@@ -1,6 +1,7 @@
 package hunkarada.nen.common.nen.ability.abstraction.ability;
 
 import hunkarada.nen.common.nen.ability.abstraction.target.TargetType;
+import hunkarada.nen.common.nen.mixin.INen;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
@@ -14,38 +15,41 @@ public abstract class TargetAbility extends Ability {
     double spellDistance;
 
     @Override
-    public void cast(LivingEntity caster, long cost) {
-        caster.collectNen();
-        getTargetByLookVector(caster);
-        switch (target.getType()){
-            case BLOCK -> {
-                if (targetType == TargetType.BLOCK || targetType == TargetType.MIXED){
-                    World world = caster.getWorld();
-                    BlockHitResult blockTarget = (BlockHitResult) target;
-                    abilityEffect.applyEffect(
+    public void cast(LivingEntity caster) {
+        if (isNotAtCooldown()){
+            // calculate cost and other things
+            prepareCast(caster);
+            // took away nen from caster
+            INen nenCaster = (INen) caster;
+            nenCaster.nen$collectNen(totalCost);
+            // getting target
+            getTargetByLookVector(caster);
+            switch (target.getType()){
+                case BLOCK -> {
+                    if (targetType == TargetType.BLOCK || targetType == TargetType.MIXED){
+                        World world = caster.getWorld();
+                        BlockHitResult blockTarget = (BlockHitResult) target;
+                        abilityEffect.applyEffect(
                             world.getBlockState(blockTarget.getBlockPos()).getBlock(), blockTarget.getType(), caster
-                    );
+                        );
+                    }
+                    else {
+                        nenCaster.nen$giveNen(totalCost);
+                    }
                 }
-                else {
-                    refundNen(caster);
-                }
-            }
-            case ENTITY -> {
-                if (targetType == TargetType.ENTITY || targetType == TargetType.MIXED){
-                    World world = caster.getWorld();
-                    EntityHitResult entityTarget = (EntityHitResult) target;
-                    abilityEffect.applyEffect(
+                case ENTITY -> {
+                        if (targetType == TargetType.ENTITY || targetType == TargetType.MIXED){
+                            EntityHitResult entityTarget = (EntityHitResult) target;
+                            abilityEffect.applyEffect(
                             ((EntityHitResult) target).getEntity(), entityTarget.getType(), caster
-                    );
-                }
-                else {
-                    refundNen(caster);
-                }
+                            );
+                        }
+                        else {
+                            nenCaster.nen$giveNen(totalCost);
+                        }
 
-            }
-            case MISS -> {
-                refundNen(caster);
-                //Notify about fail here.
+                }
+                case MISS -> nenCaster.nen$giveNen(totalCost);
             }
         }
     }
