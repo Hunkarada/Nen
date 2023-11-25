34f6534260804cab6b6c6ea12278e4c3e02463fc
Hunkarada <hfsihtr@gmail.com>
2023-11-25 13:57:43 +0500

    now registry returns new instance of object with default params

--- src/main/java/hunkarada/nen/common/nen/ability/registry/AbilityRegistry.java
+++ src/main/java/hunkarada/nen/common/nen/ability/registry/AbilityRegistry.java
@@ -3,6 +3,7 @@ package hunkarada.nen.common.nen.ability.registry;
 import hunkarada.nen.common.nen.ability.abstraction.ability.Ability;
 import hunkarada.nen.common.abstractions.Registry;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class AbilityRegistry implements Registry<Ability> {
@@ -19,6 +20,11 @@ public class AbilityRegistry implements Registry<Ability> {
         Ability value = registry.get(key);
         if (value == null){
             throw new NullPointerException("You should register Ability");
+        }
+        try {
+            value = value.getClass().getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
         return value;
     }
--- src/main/java/hunkarada/nen/common/nen/ability/registry/EffectRegistry.java
+++ src/main/java/hunkarada/nen/common/nen/ability/registry/EffectRegistry.java
@@ -3,6 +3,7 @@ package hunkarada.nen.common.nen.ability.registry;
 import hunkarada.nen.common.nen.ability.abstraction.ability.AbilityEffect;
 import hunkarada.nen.common.abstractions.Registry;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class EffectRegistry implements Registry<AbilityEffect> {
@@ -21,6 +22,11 @@ public class EffectRegistry implements Registry<AbilityEffect> {
         AbilityEffect value = registry.get(key);
         if (value == null){
             throw new NullPointerException("You should register AbilityEffect");
+        }
+        try {
+            value = value.getClass().getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
         return value;
     }
