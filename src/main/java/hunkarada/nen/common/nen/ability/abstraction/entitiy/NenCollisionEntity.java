5db71298f0f4de0f0009e1f4382f6ece1cccacf4
Hunkarada <hfsihtr@gmail.com>
2023-12-24 16:30:28 +0500

    add logic of collision entity

--- src/main/java/hunkarada/nen/common/nen/ability/abstraction/entitiy/NenCollisionEntity.java
+++ src/main/java/hunkarada/nen/common/nen/ability/abstraction/entitiy/NenCollisionEntity.java
@@ -1,31 +1,72 @@
 package hunkarada.nen.common.nen.ability.abstraction.entitiy;
 
-import hunkarada.nen.common.nen.ability.abstraction.shape.Shape;
+import hunkarada.nen.common.nen.IEntityNen;
+import hunkarada.nen.common.nen.ability.abstraction.ability.AbilityEffect;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class NenCollisionEntity extends Entity {
-
+    Box collisionBox;
+    AbilityEffect abilityEffect;
     int lifetime;
-    Shape shape;
-    Vec3d[] positions;
-
-    float radius;
-
+    double radius;
+    boolean isSphere;
+    boolean isMass;
     public static final EntityType<NenCollisionEntity> NEN_COLLISION_ENTITY = null; // need to register that
 
-    public NenCollisionEntity(EntityType<?> type, World world) {
+    public NenCollisionEntity(EntityType<?> type, World world, Box collisionBox, int lifetime, AbilityEffect abilityEffect) {
         super(type, world);
+        this.collisionBox = collisionBox;
+        this.lifetime = lifetime;
+        this.abilityEffect = abilityEffect;
+        calcRadius();
     }
 
+    public void calcRadius(){
+        radius = ((Math.max(collisionBox.getXLength(), collisionBox.getZLength())) / 2);
+    }
+
+    // Here we check for any collision with entities.
+    // If we found more than one entity,
+    // we cast ability on the closest entity or on all of them (depends on @param isMass)
     protected void checkForCollision(){
-        switch (shape){
-            case CUBE -> {} // positions
-            case SPHERE -> {} // radius
+        List<Entity> entityList = this.world.getOtherEntities(this, collisionBox);
+        if (!entityList.isEmpty()) {
+            Vec3d center = collisionBox.getCenter();
+            List<Entity> filteredList = new ArrayList<>();
+            if (isSphere) {
+                for (Entity entity : entityList) {
+                    if (Math.sqrt(entity.getBlockPos().getSquaredDistance(center)) <= radius) {
+                        filteredList.add(entity);
+                    }
+                }
+            } else {
+                filteredList.addAll(entityList);
+            }
+            if (isMass) {
+                for (Entity entity : filteredList) {
+                    IEntityNen entityNen = (IEntityNen) entity;
+                    entityNen.nen$addNenAbilityEffect(abilityEffect);
+                }
+            } else {
+                Entity closestEntity = filteredList.get(0);
+                for (Entity entity : filteredList) {
+                    if (entity.getBlockPos().getSquaredDistance(center) < closestEntity.getBlockPos().getSquaredDistance(center)) {
+                        closestEntity = entity;
+                    }
+                }
+                IEntityNen entityNen = (IEntityNen) closestEntity;
+                entityNen.nen$addNenAbilityEffect(abilityEffect);
+            }
         }
+
+
     }
     @Override
     public void tick(){
