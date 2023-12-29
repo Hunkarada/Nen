c8a660a0ddfac380ed7ffbf4ba3345d8a6287d6f
Brilliance-DM <sobakaidi@mail.ru>
2023-12-29 14:18:50 +0500

    CAST_PACKET_ID redact

--- src/main/java/hunkarada/nen/common/network/ModMessages.java
+++ src/main/java/hunkarada/nen/common/network/ModMessages.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier;
 
 public class ModMessages {
 
-    public static final Identifier CAST_PACKET_ID = new Identifier(NenMod.MOD_ID, "cast_packet")
+    public static final Identifier CAST_PACKET_ID = new Identifier(NenMod.MOD_ID, "cast_packet");
 
     public static void registerC2SPackets(){
         ServerPlayNetworking.registerGlobalReceiver(CAST_PACKET_ID, CastPacket::receive);
