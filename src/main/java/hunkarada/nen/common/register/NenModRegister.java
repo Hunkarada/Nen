a43b528b536a353262813c5b7eb429de336e2234
Hunkarada <hfsihtr@gmail.com>
2023-12-29 18:56:17 +0500

    made registry value of entities accessible

--- src/main/java/hunkarada/nen/common/register/NenModRegister.java
+++ src/main/java/hunkarada/nen/common/register/NenModRegister.java
@@ -11,6 +11,7 @@ import hunkarada.nen.common.nen.ability.abstraction.entitiy.NenProjectileEntity;
 import hunkarada.nen.common.network.ModMessages;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -41,13 +42,16 @@ public class NenModRegister {
         new EmptyAbilitySet().register();
         new CreatorAbilitySet().register();
     }
+    public static EntityType<NenAbilityEntity> NEN_ABILITY_ENTITY;
+    public static EntityType<NenCollisionEntity> NEN_COLLISION_ENTITY;
+    public static EntityType<NenProjectileEntity> NEN_PROJECTILE_ENTITY;
     //works for both sides
     public static void registerEntities(){
-        Registry.register(Registries.ENTITY_TYPE, NenAbilityEntity.NEN_ABILITY_ENTITY_ID,
+        NEN_ABILITY_ENTITY = Registry.register(Registries.ENTITY_TYPE, NenAbilityEntity.NEN_ABILITY_ENTITY_ID,
                 FabricEntityTypeBuilder.create(SpawnGroup.MISC, NenAbilityEntity::new).dimensions(EntityDimensions.fixed(0, 0)).build());
-        Registry.register(Registries.ENTITY_TYPE, NenCollisionEntity.NEN_COLLISION_ENTITY_ID,
+        NEN_COLLISION_ENTITY = Registry.register(Registries.ENTITY_TYPE, NenCollisionEntity.NEN_COLLISION_ENTITY_ID,
                 FabricEntityTypeBuilder.create(SpawnGroup.MISC, NenCollisionEntity::new).dimensions(EntityDimensions.fixed(0,0)).build());
-        Registry.register(Registries.ENTITY_TYPE, NenProjectileEntity.NEN_PROJECTILE_ENTITY,
+        NEN_PROJECTILE_ENTITY = Registry.register(Registries.ENTITY_TYPE, NenProjectileEntity.NEN_PROJECTILE_ENTITY,
                 FabricEntityTypeBuilder.create(SpawnGroup.MISC, NenProjectileEntity::new).dimensions(EntityDimensions.fixed(0, 0)).build());
     }
 
