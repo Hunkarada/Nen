52f5d231ba9a6cf3f2bafd0eef1ecd1aca1c9b76
Hunkarada <hfsihtr@gmail.com>
2024-01-16 19:17:51 +0500

    add validation of awakening caster

--- src/main/java/hunkarada/nen/common/network/packet/CastPacket.java
+++ src/main/java/hunkarada/nen/common/network/packet/CastPacket.java
@@ -8,14 +8,16 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class CastPacket {
-
     public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler,
                                PacketByteBuf buf, PacketSender responseSender)
     {
-        int value = buf.getInt(0);
-        if (value >= 0 && value <= 4){
-            IPlayerEntityNen nenPlayer = (IPlayerEntityNen) player;
-            nenPlayer.nen$getNenAbilities().getAbilitySetCopy().get(value).cast(player);
+        // validate is player available to cast abilities and validate range of a packet is 0-4.
+        IPlayerEntityNen nenPlayer = (IPlayerEntityNen) player;
+        if (nenPlayer.nen$getIsNenAwakened()) {
+            int value = buf.readInt();
+            if (value >= 0 && value <= 4) {
+                nenPlayer.nen$getNenAbilities().getAbilitySetCopy().get(value).cast(player);
+            }
         }
     }
 }
