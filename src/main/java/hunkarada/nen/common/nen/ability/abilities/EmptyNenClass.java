87ffcc88a4aa8c05fbcbd7e7e9ff6a155119e912
Hunkarada <hfsihtr@gmail.com>
2024-01-29 18:57:39 +0500

    fixed forgotten test of empty class

--- src/main/java/hunkarada/nen/common/nen/ability/abilities/EmptyNenClass.java
+++ src/main/java/hunkarada/nen/common/nen/ability/abilities/EmptyNenClass.java
@@ -1,6 +1,5 @@
 package hunkarada.nen.common.nen.ability.abilities;
 
-import hunkarada.nen.common.nen.ability.abilities.conjuration.creator.selectblockability.SelectBlockAbility;
 import hunkarada.nen.common.nen.ability.abstraction.ability.NenClass;
 
 import java.util.ArrayList;
@@ -8,7 +7,7 @@ import java.util.ArrayList;
 public class EmptyNenClass extends NenClass {
     public EmptyNenClass(){
         id = "empty_nen_class";
-        classAbility = new SelectBlockAbility();
+        ultimateClassAbility = new EmptyAbility();
         classAvailableAbilities = new ArrayList<>();
         classAvailableAbilities.add(new EmptyAbility());
         classAvailableAbilities.add(new EmptyAbility());
