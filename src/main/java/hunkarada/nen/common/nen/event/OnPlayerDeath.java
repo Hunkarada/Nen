fb9654981b34631cc3b763b62d0f1db3396e613f
Hunkarada <hfsihtr@gmail.com>
2024-03-31 23:40:17 +0500

    made resetting of an obvious need to reset values on player death

--- src/main/java/hunkarada/nen/common/nen/event/OnPlayerDeath.java
+++ src/main/java/hunkarada/nen/common/nen/event/OnPlayerDeath.java
@@ -1,5 +1,6 @@
 package hunkarada.nen.common.nen.event;
 
+import hunkarada.nen.common.nen.IEntityNen;
 import hunkarada.nen.common.nen.IPlayerEntityNen;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.minecraft.nbt.NbtCompound;
@@ -11,9 +12,24 @@ public class OnPlayerDeath implements ServerPlayerEvents.CopyFrom{
         IPlayerEntityNen oldNenPlayer = (IPlayerEntityNen) oldPlayer;
         IPlayerEntityNen newNenPlayer = (IPlayerEntityNen) newPlayer;
         NbtCompound nenPlayerData = oldNenPlayer.nen$saveDataToNbt();
-        // setting nen to zero.
+        // resetting some part of nen values after death on player part
         nenPlayerData.putDouble("nenPower", 0);
+        nenPlayerData.putBoolean("isNenActive", false);
+        nenPlayerData.putBoolean("isNenHidden", false);
+        nenPlayerData.putBoolean("isCanSeeNen", false);
+        nenPlayerData.putBoolean("isNenBlocked", false);
+        nenPlayerData.putInt("nenBlockedTime", 0);
 
         newNenPlayer.nen$loadDataFromNbt(nenPlayerData);
+        newNenPlayer.nen$updateAttributes();
+
+        // same, but for entity part
+        IEntityNen oldEntityPlayer = (IEntityNen) oldPlayer;
+        IEntityNen newEntityPlayer = (IEntityNen) newPlayer;
+
+        NbtCompound oldNenEntityData = oldEntityPlayer.nen$saveNenEntityToNbt();
+        oldNenEntityData.put("nenAbilityEffects", new NbtCompound());
+        oldNenEntityData.put("nenMemory", new NbtCompound());
+        newEntityPlayer.nen$loadNenEntityFromNbt(oldNenEntityData);
     }
 }
