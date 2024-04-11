b5d66a4ae48aee5bc81b39abd7f4512862b07222
Hunkarada <hfsihtr@gmail.com>
2024-04-11 18:27:22 +0500

    removed test ability from class

--- src/main/java/hunkarada/nen/common/nen/ability/abilities/conjuration/creator/CreatorNenClass.java
+++ src/main/java/hunkarada/nen/common/nen/ability/abilities/conjuration/creator/CreatorNenClass.java
@@ -1,6 +1,5 @@
 package hunkarada.nen.common.nen.ability.abilities.conjuration.creator;
 
-import hunkarada.nen.common.nen.ability.abilities.conjuration.creator.selectblockability.SelectBlockAbility;
 import hunkarada.nen.common.nen.ability.abstraction.ability.NenClass;
 
 import java.util.ArrayList;
@@ -8,6 +7,5 @@ import java.util.ArrayList;
 public class CreatorNenClass extends NenClass {
    CreatorNenClass(){
        classAbilities = new ArrayList<>();
-       classAbilities.set(4, new SelectBlockAbility());
    }
 }
